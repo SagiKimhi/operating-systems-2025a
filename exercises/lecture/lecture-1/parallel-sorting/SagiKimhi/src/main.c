753716995c19f8e0a34f4a84e9ea191a3f9f9592
Sagi Kimhi <sagi.kim5@gmail.com>
2024-11-09 06:27:48 +0200

    exercise(chapter2): improve fork_join solution
    Signed-off-by: Sagi Kimhi <sagi.kim5@gmail.com>

---
+++ exercises/lecture/lecture-1/parallel-sorting/SagiKimhi/src/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stddef.h>
+#include <unistd.h>
+#include <sys/types.h>
+
+void parallel_sort(char **argv, char arr, size_t max);
+
+int main(int argc, char *argv[])
+{
+	fprintf(stdout, "Hello, World!\n");
+    (void)(argc);
+    (void)(argv);
+	return 0;
+}
+
